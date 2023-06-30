86c2c0be464403d52fa8f2ca1d919762ce3fb75b
qpwoei0123 <qpwoei0123@naver.com>
2023-06-30 17:14:10 +0900

    style: 프리티어 적용

--- client/src/pages/MainPage.tsx
+++ client/src/pages/MainPage.tsx
@@ -1,5 +1,5 @@
 const MainPage = () => {
-    return <div>MainPage</div>;
-  };
-  
+  return <div>MainPage</div>;
+};
+
 export default MainPage;
--- client/src/routes/Router.tsx
+++ client/src/routes/Router.tsx
@@ -1,17 +1,15 @@
-import { createBrowserRouter } from "react-router-dom";
-import App from "../App";
-import MainPage from "../pages/MainPage";
-import NotFound from "../error/NotFound";
+import { createBrowserRouter } from 'react-router-dom';
+import App from '../App';
+import MainPage from '../pages/MainPage';
+import NotFound from '../error/NotFound';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
     errorElement: <NotFound />,
-    children: [
-      { index: true, element: <MainPage /> },
-    ]
-  }
+    children: [{ index: true, element: <MainPage /> }],
+  },
 ]);
 
 export default router;
