586d916fae38677bef64f55a1f78984bfd69371c
qpwoei0123 <qpwoei0123@naver.com>
2023-06-30 15:19:52 +0900

    chore: 이니셜 세팅

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import MainPage from "./pages/MainPage"
+
+function App() {
+  return (
+    <div className="App">
+      <MainPage/>
+    </div>
+  );
+}
+
+export default App;
