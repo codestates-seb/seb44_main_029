419f42a2676a2baa1f0bc646c8c113871610683d
dlwlgy <dlwlgy81@gmail.com>
2023-06-30 15:46:07 +0900

    chore: 라우터 초기 설정

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import MainPage from "./pages/MainPage"
+import { Outlet } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <MainPage/>
-    </div>
+    <>
+      <Outlet />
+    </>
   );
 }
 
---
+++ client/src/routes/Router.tsx
@@ -0,0 +1,17 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from "../App";
+import MainPage from "../pages/MainPage";
+import NotFound from "../error/NotFound";
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [
+      { index: true, element: <MainPage /> },
+    ]
+  }
+]);
+
+export default router;
