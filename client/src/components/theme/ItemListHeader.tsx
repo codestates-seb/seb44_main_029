fe6aec5555e2c8ec0f9779eaab2104d7031f0f16
dlwlgy <dlwlgy81@gmail.com>
2023-07-05 10:02:37 +0900

    design: ThemeHeader, ItemListHeader UI 디자인 변경

--- client/src/components/theme/ItemListHeader.tsx
+++ client/src/components/theme/ItemListHeader.tsx
@@ -6,7 +6,7 @@ const ThemeContent = () => {
     <Container>
       <ListFilterDiv>
         <h3 className="item-list-title">Item List</h3>
-        <button type="button">좋아요 🤍</button>
+        <button type="button">좋아요 ❤️</button>
       </ListFilterDiv>
     </Container>
   );
@@ -20,31 +20,30 @@ const Container = styled.div`
 `;
 
 const ListFilterDiv = styled.div`
-  background-color: rgba(0, 0, 0, 0.88);
   width: 100%;
   display: flex;
   justify-content: space-between;
   align-items: center;
-  border-bottom: 1px solid rgba(255, 255, 255, 1);
+  background-color: rgba(255, 255, 255, 0.05);
 
   .item-list-title {
-    margin: 1rem 2rem;
+    margin: 1rem 3rem;
     color: rgba(255, 255, 255, 1);
   }
 
   > button {
     width: 3.7rem;
     height: 1.5rem;
-    margin-right: 1rem;
+    margin-right: 3rem;
     transition: 0.3s;
-    border: 2px solid #c8c8c8;
+    border: 2px solid rgba(255, 255, 255, 1);
     border-radius: 0.4rem;
     background-color: transparent;
-    color: rgba(255, 255, 255, 0.7);
+    color: rgba(255, 255, 255, 1);
     font-size: 0.6rem;
 
     &:hover {
-      background-color: #474747;
+      background-color: rgba(255, 255, 255, 0.2);
     }
   }
 `;
