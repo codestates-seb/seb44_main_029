e79d61bca4a31a28ee8204a57728042ddeda7fe6
HEECHANG96 <wjdgmlckd96@daum.net>
2023-07-06 09:33:15 +0900

    style: 메인 페이지 CSS 수정

--- client/src/pages/MainPage.tsx
+++ client/src/pages/MainPage.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import backgroundImg from '../assets/images/mainPage_background.jpg';
 import Title from '../components/main/Title';
+
 const MainPage = () => {
   return (
     <Layout>
@@ -17,10 +18,7 @@ const Layout = styled.div`
   height: 100vh;
   padding: 0;
   display: flex;
-  justify-content: start;
   align-items: center;
-  position: relative;
-  overflow: hidden;
   background-image: url(${backgroundImg});
   background-repeat: no-repeat;
   background-position: center;
