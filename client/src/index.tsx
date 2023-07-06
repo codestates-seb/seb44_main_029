9925e4bddbeaf572a12dc1333799595078b51014
dlwlgy <dlwlgy81@gmail.com>
2023-07-06 14:12:19 +0900

    Feat: TypeScript + RTK 기본 구조 구현, API 요청이 가능한 시점에 추가 구현 및 수정 계획

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,13 +4,17 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 import { RouterProvider } from 'react-router-dom';
 import router from './routes/Router';
+import { Provider } from 'react-redux';
+import store from './store/store';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
 
