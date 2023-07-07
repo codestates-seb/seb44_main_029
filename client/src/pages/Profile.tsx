b9c35487216de82f046d324e880ea4fa170d66ea
HEECHANG96 <wjdgmlckd96@daum.net>
2023-07-07 22:50:51 +0900

    feat: 프로필 페이지 상단 구조 구현

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -1,5 +1,49 @@
+import styled from 'styled-components';
+import backgroundImg from '../assets/images/profile_background.png';
+import ChangeProfile from '../components/profile/ChangeProfile';
+
 const Profile = () => {
-  return <>Profile</>;
+  return (
+    <Layout>
+      <Container>
+        <ChangeProfile />
+      </Container>
+    </Layout>
+  );
 };
 
 export default Profile;
+
+const Layout = styled.div`
+  max-width: 100%;
+  width: 100vw;
+  height: 100vh;
+  padding: 6rem 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: relative;
+  overflow: hidden;
+
+  &::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-image: url(${backgroundImg});
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
+    filter: blur(5px);
+    z-index: -1;
+    transform: scale(1.02);
+  }
+`;
+
+const Container = styled.div`
+  width: 924px;
+  height: 800px;
+  background-color: rgba(0, 0, 0, 0.3);
+`;
