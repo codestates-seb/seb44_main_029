bbc0a154c58c6017c637e3f7db6d1a4c9e4a7aef
dlwlgy <dlwlgy81@gmail.com>
2023-07-07 14:54:08 +0900

    feat: 좋아요 버튼 클릭 시, 색 전환되는 스타일 구현. 필터링 기능은 API 명세서 확인 후 추가 구현 예정

--- client/src/components/theme/ItemListHeader.tsx
+++ client/src/components/theme/ItemListHeader.tsx
@@ -1,18 +1,35 @@
-import React from 'react';
-import styled from 'styled-components';
+import React, { useState } from 'react';
+import styled, { css } from 'styled-components';
 
-const ThemeContent = () => {
+interface ButtonProps {
+  isActive: boolean;
+}
+
+const ItemListHeader = () => {
+  const [showLikedOnly, setShowLikedOnly] = useState<boolean>(false);
+
+  const handleLikeButtonClick = (): void => {
+    setShowLikedOnly(!showLikedOnly);
+  };
+
   return (
     <Container>
       <ListFilterDiv>
         <h3 className="item-list-title">Item List</h3>
-        <button type="button">좋아요 ❤️</button>
+        <LikeFilterButton
+          type="button"
+          isActive={showLikedOnly}
+          onClick={handleLikeButtonClick}
+        >
+          좋아요 <br />
+          {showLikedOnly ? '❤️' : '🤍'}
+        </LikeFilterButton>
       </ListFilterDiv>
     </Container>
   );
 };
 
-export default ThemeContent;
+export default ItemListHeader;
 
 const Container = styled.div`
   width: 100%;
@@ -30,20 +47,34 @@ const ListFilterDiv = styled.div`
     margin: 1rem 3rem;
     color: rgba(255, 255, 255, 1);
   }
+`;
 
-  > button {
-    width: 3.7rem;
-    height: 1.5rem;
-    margin-right: 3rem;
-    transition: 0.3s;
-    border: 2px solid rgba(255, 255, 255, 1);
-    border-radius: 0.4rem;
-    background-color: transparent;
-    color: rgba(255, 255, 255, 1);
-    font-size: 0.6rem;
+const LikeFilterButton = styled.button<ButtonProps>`
+  width: 3rem;
+  height: 1.5rem;
+  margin-right: 3rem;
+  transition: 0.3s;
+  border-radius: 0.4rem;
+  background-color: transparent;
+  color: rgba(255, 255, 255, 1);
+  font-size: 0.6rem;
+  letter-spacing: 1px;
+  line-height: 14.5px;
+  /* border: 2px solid rgba(255, 255, 255, 1);
+  box-shadow: 0 0 0.2rem 0.1rem rgba(255, 255, 255, 1); */
 
-    &:hover {
-      background-color: rgba(255, 255, 255, 0.2);
-    }
+  &:hover {
+    background-color: rgba(255, 255, 255, 0.1);
   }
+
+  ${(props) =>
+    props.isActive
+      ? css`
+          border: 2px solid rgba(255, 0, 0, 1);
+          box-shadow: 0 0 0.2rem 0.1rem rgba(255, 0, 0, 1);
+        `
+      : css`
+          border: 2px solid rgba(255, 255, 255, 1);
+          box-shadow: 0 0 0.2rem 0.1rem rgba(255, 255, 255, 1);
+        `}
 `;
