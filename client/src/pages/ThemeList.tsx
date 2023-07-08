0e33dbb1d4727acbe384699880ba0c594c8ac703
qpwoei0123 <qpwoei0123@naver.com>
2023-07-08 17:34:04 +0900

    feat: 캐러셀 구조구현

--- client/src/pages/ThemeList.tsx
+++ client/src/pages/ThemeList.tsx
@@ -1,5 +1,28 @@
+import ThemeCarousel from '../components/theme/themeList/ThemeCarousel';
+import styled from 'styled-components';
 const ThemeList = () => {
-  return <>asd</>;
+  return (
+    <Layout>
+      <ThemeCarousel />
+      <TextContainer />
+    </Layout>
+  );
 };
 
 export default ThemeList;
+
+const Layout = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100vw;
+  height: 100vh;
+  background-color: gray;
+`;
+
+const TextContainer = styled.section`
+  width: 100vw;
+  height: 20vh;
+  border: 1px solid;
+`;
