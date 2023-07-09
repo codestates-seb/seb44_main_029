99bc683afe36c00345335f7d1baca678c3ddf047
qpwoei0123 <qpwoei0123@naver.com>
2023-07-09 13:21:08 +0900

    feat: 테마 배경이미지 구현

--- client/src/components/theme/themeList/ThemeCarousel.tsx
+++ client/src/components/theme/themeList/ThemeCarousel.tsx
@@ -1,13 +1,18 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
-import img1 from '../../../assets/theme/1.png';
-import img2 from '../../../assets/theme/2.png';
-import img3 from '../../../assets/theme/3.png';
 
-const ThemeCarousel = () => {
-  const [currentThemeIndex, setCurrentThemeIndex] = useState(0);
+interface ThemeCarouselProps {
+  imgList: string[];
+  currentThemeIndex: number;
+  setCurrentThemeIndex: React.Dispatch<React.SetStateAction<number>>;
+}
 
-  const themes = [img1, img2, img3];
+const ThemeCarousel = ({
+  imgList,
+  currentThemeIndex,
+  setCurrentThemeIndex,
+}: ThemeCarouselProps) => {
+  const themes: string[] = imgList;
 
   const handleNextTheme = () => {
     setCurrentThemeIndex((prevIndex) => (prevIndex + 1) % themes.length);
@@ -39,26 +44,27 @@ const ThemeCarousel = () => {
 export default ThemeCarousel;
 
 const Container = styled.section`
-  height: 50vh;
+  height: 40vh;
   width: 100vw;
   display: flex;
   justify-content: center;
 `;
 
 const CenteredContainer = styled.div`
-  width: 60vw;
+  width: 70vw;
   height: 100%;
   display: flex;
   justify-content: center;
+  border-radius: 20px;
+  box-shadow: 0 0 10px 5px rgba(0, 0, 0, 0.5);
 `;
 
 const PreviousThemeImg = styled.img`
   width: 10vw;
   height: auto;
   object-fit: cover;
-  opacity: 0.5;
   border-radius: 0 20px 20px 0;
-  transition: opacity 0.3s ease-in-out;
+  box-shadow: 0 0 10px 5px rgba(0, 0, 0, 0.5);
 `;
 
 const CurrentThemeImg = styled.img`
@@ -67,16 +73,12 @@ const CurrentThemeImg = styled.img`
   object-fit: cover;
   margin: 0 10vw;
   border-radius: 20px;
-  transition: margin 0.3s ease-in-out;
-  transform-style: preserve-3d;
-  transform: perspective(1000px);
 `;
 
 const NextThemeImg = styled.img`
   width: 10vw;
   height: auto;
   object-fit: cover;
-  opacity: 0.5;
   border-radius: 20px 0 0 20px;
-  transition: opacity 0.3s ease-in-out;
+  box-shadow: 0 0 10px 5px rgba(0, 0, 0, 0.5);
 `;
--- client/src/pages/ThemeList.tsx
+++ client/src/pages/ThemeList.tsx
@@ -1,9 +1,25 @@
 import ThemeCarousel from '../components/theme/themeList/ThemeCarousel';
 import styled from 'styled-components';
+import img1 from '../assets/theme/1.png';
+import img2 from '../assets/theme/2.png';
+import img3 from '../assets/theme/3.png';
+import img4 from '../assets/theme/4.png';
+import img5 from '../assets/theme/5.png';
+import { useState } from 'react';
+
 const ThemeList = () => {
+  const imgList = [img1, img2, img3, img4, img5];
+  const [currentThemeIndex, setCurrentThemeIndex] = useState(0);
+  const currentThemeImageUrl = imgList[currentThemeIndex];
+
   return (
     <Layout>
-      <ThemeCarousel />
+      <BlurredBackground imageUrl={currentThemeImageUrl} />
+      <ThemeCarousel
+        imgList={imgList}
+        currentThemeIndex={currentThemeIndex}
+        setCurrentThemeIndex={setCurrentThemeIndex}
+      />
       <TextContainer />
     </Layout>
   );
@@ -12,17 +28,30 @@ const ThemeList = () => {
 export default ThemeList;
 
 const Layout = styled.div`
+  position: relative;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
   width: 100vw;
   height: 100vh;
-  background-color: gray;
+`;
+
+const BlurredBackground = styled.div<{ imageUrl: string }>`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-image: url(${(props) => props.imageUrl});
+  background-size: cover;
+  background-repeat: no-repeat;
+  filter: blur(5px);
+  z-index: -1;
 `;
 
 const TextContainer = styled.section`
-  width: 100vw;
+  width: 60vw;
   height: 20vh;
   border: 1px solid;
 `;
