b8d1bc7e82cd4f39a8c96ac1d85bb3588eaaa3b4
Lee dong yeol <dlehdduf09020@naver.com>
2023-07-12 10:48:52 +0900

    음원 업로드 기능 구현

--- src/main/java/com/example/server/theme/controller/ThemeController.java
+++ src/main/java/com/example/server/theme/controller/ThemeController.java
@@ -12,9 +12,7 @@ public class ThemeController {
     // 전체 테마 리스트 반환 > 저장된 테마들의 기본 이미지를 리스트 형태로 전달
     @GetMapping
     public ResponseEntity getThemes(){
-        // 저장소에서 이미지 불러오는 로직 > 이걸 mysql로 진행하면 허웅님과
-        // 겹칠 것 같아 resource 파일에서 가져오는 로직으로 변경.
-        //
+
     return null;
 
     }
