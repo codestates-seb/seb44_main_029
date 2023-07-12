090146c7f9b00e0fb72a90280e195001ce74bda1
qpwoei0123 <qpwoei0123@naver.com>
2023-07-12 13:48:06 +0900

    feat: prfile/edit EditImg 컴포넌트 구조 구현

--- client/src/pages/ProfileEdit.tsx
+++ client/src/pages/ProfileEdit.tsx
@@ -1,5 +1,13 @@
-const ProfileEdit = () => {
-  return <div>Profile Edit Page</div>;
-};
+import { Layout, ContentContainer } from './Profile';
+import EditImg from '../components/profile/edit/EditImg';
 
+const ProfileEdit = () => {
+  return (
+    <Layout>
+      <ContentContainer>
+        <EditImg />
+      </ContentContainer>
+    </Layout>
+  );
+};
 export default ProfileEdit;
