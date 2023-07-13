6dcae5408c565030b3c476a637bfe1fa800197f7
qpwoei0123 <qpwoei0123@naver.com>
2023-07-13 10:14:54 +0900

    Refactor: API BASE_URL 추가

--- client/src/api/api.ts
+++ client/src/api/api.ts
@@ -7,28 +7,24 @@ import {
   PageInfo,
 } from '../types/types';
 
+const BASE_URL = 'https://3a11-175-208-216-56.ngrok-free.app/';
 /* 유저 정보 가져오기 */
 export const GetMusic = (ThemeId: string | undefined): Promise<Musics> =>
   axios
-    .get(
-      `https://aace-175-208-216-56.ngrok-free.app/theme/${ThemeId}/music/list`,
-      {
-        headers: {
-          'Content-Type': 'application/json',
-          'ngrok-skip-browser-warning': '69420',
-        },
-      }
-    )
+    .get(`${BASE_URL}theme/${ThemeId}/music/list`, {
+      headers: {
+        'Content-Type': 'application/json',
+        'ngrok-skip-browser-warning': '69420',
+      },
+    })
     .then((res) => res.data);
 
 export const SignUp = (data: SignUpInfo) =>
-  axios
-    .post('https://aace-175-208-216-56.ngrok-free.app/members', data)
-    .then((res) => res.data);
+  axios.post(`${BASE_URL}members`, data).then((res) => res.data);
 
 export const Login = async (data: LoginInfo) => {
   const response = await axios.post(
-    'https://aace-175-208-216-56.ngrok-free.app/members/login',
+    `${BASE_URL}members/login`,
     {
       email: data.email,
       password: data.password,
@@ -48,18 +44,14 @@ export const Logout = async () => {
   const accessToken = localStorage.getItem('accessToken');
   const refreshToken = localStorage.getItem('refreshToken');
 
-  const response = await axios.post(
-    'https://aace-175-208-216-56.ngrok-free.app/members/logout',
-    null,
-    {
-      headers: {
-        'Content-Type': 'application/json',
-        'ngrok-skip-browser-warning': '69420',
-        accessToken: `Bearer ${accessToken}`,
-        refreshToken: refreshToken,
-      },
-    }
-  );
+  const response = await axios.post(`${BASE_URL}members/logout`, null, {
+    headers: {
+      'Content-Type': 'application/json',
+      'ngrok-skip-browser-warning': '69420',
+      accessToken: `Bearer ${accessToken}`,
+      refreshToken: refreshToken,
+    },
+  });
 
   return response;
 };
