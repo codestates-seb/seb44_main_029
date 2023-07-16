5aa9ea52e1308555dd4ab00768b3b66900c0789b
dlwlgy <dlwlgy81@gmail.com>
2023-07-16 16:41:08 +0900

    feat: 상세 이미지 페이지로 가는 경로 추가

--- client/src/routes/Router.tsx
+++ client/src/routes/Router.tsx
@@ -6,6 +6,7 @@ import ThemeItemList from '../pages/ThemeItemList';
 import ThemeList from '../pages/ThemeList';
 import Profile from '../pages/Profile';
 import ProfileEdit from '../pages/ProfileEdit';
+import ThemeDetailedItem from '../pages/ThemeDetailedItem';
 
 const router = createBrowserRouter([
   {
@@ -15,12 +16,16 @@ const router = createBrowserRouter([
     children: [
       { index: true, element: <MainPage /> },
       {
+        path: '/theme',
+        element: <ThemeList />,
+      },
+      {
         path: '/theme/:themeId',
         element: <ThemeItemList />,
       },
       {
-        path: '/theme',
-        element: <ThemeList />,
+        path: '/theme/:themeId/:contentId',
+        element: <ThemeDetailedItem />,
       },
       {
         path: '/profile',
