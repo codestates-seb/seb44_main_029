95389d0e83e4b670f3c54e7c8d291592f82cd435
Lee dong yeol <dlehdduf09020@naver.com>
2023-07-17 18:45:28 +0900

    build 완료 직전

--- build/generated/sources/annotationProcessor/java/main/com/example/server/member/mapper/MemberMapperImpl.java
+++ build/generated/sources/annotationProcessor/java/main/com/example/server/member/mapper/MemberMapperImpl.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.example.server.member.Mapper;
 
 import com.example.server.member.dto.MemberResponseDto;
@@ -8,8 +7,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-07-14T16:41:31+0900",
-    comments = "version: 1.5.1.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-8.1.1.jar, environment: Java 11.0.18 (Azul Systems, Inc.)"
+    date = "2023-07-17T15:11:03+0900",
+    comments = "version: 1.5.3.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-8.1.1.jar, environment: Java 11.0.15 (Amazon.com Inc.)"
 )
 @Component
 public class MemberMapperImpl implements MemberMapper {
@@ -29,35 +28,3 @@ public class MemberMapperImpl implements MemberMapper {
         return memberResponseDto.build();
     }
 }
-=======
-package com.example.server.member.Mapper;
-
-import com.example.server.member.dto.MemberResponseDto;
-import com.example.server.member.entity.Member;
-import javax.annotation.processing.Generated;
-import org.springframework.stereotype.Component;
-
-@Generated(
-    value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-07-17T08:40:36+0900",
-    comments = "version: 1.5.1.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-8.1.1.jar, environment: Java 11.0.18 (Azul Systems, Inc.)"
-)
-@Component
-public class MemberMapperImpl implements MemberMapper {
-
-    @Override
-    public MemberResponseDto memberToMemberResponseDto(Member member) {
-        if ( member == null ) {
-            return null;
-        }
-
-        MemberResponseDto.MemberResponseDtoBuilder memberResponseDto = MemberResponseDto.builder();
-
-        memberResponseDto.username( member.getUsername() );
-        memberResponseDto.email( member.getEmail() );
-        memberResponseDto.imageUrl( member.getImageUrl() );
-
-        return memberResponseDto.build();
-    }
-}
->>>>>>> d2dc19846a2070ef5ccb2dd6b92ca60fb057bfc6
