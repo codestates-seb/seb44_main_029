d08bfb49b539edeab5519701ecec35bea6d2e71e
qpwoei0123 <qpwoei0123@naver.com>
2023-07-17 13:38:51 +0900

    Feat: 메인 페이지 버튼

--- client/src/components/main/Title.tsx
+++ client/src/components/main/Title.tsx
@@ -1,14 +1,25 @@
 import styled, { keyframes } from 'styled-components';
+import { TbCarouselHorizontal, TbLogin } from 'react-icons/tb';
 
 const Title = () => {
   return (
     <Container>
       <TitleDiv>
-        <p>안정감과 편안함을 제공하는</p>
+        <p>편안함을 제공하는</p>
         <Column>
           <p>미디어 서비스 </p>
           <p className="fadeIn">Cozy State</p>
         </Column>
+        <BtnColumnDiv>
+          <button>
+            <TbLogin />
+            로그인
+          </button>
+          <button>
+            <TbCarouselHorizontal />
+            테마 둘러보기
+          </button>
+        </BtnColumnDiv>
       </TitleDiv>
     </Container>
   );
@@ -18,10 +29,32 @@ export default Title;
 
 //페이드 아웃 애니메이션
 const fadeInAnimation = keyframes`
-  from {
+  0% {
     opacity: 0;
   }
-  to {
+  25% {
+    opacity: 1;
+  }
+  70% {
+    color: white;
+  }
+
+  80%{
+    color: #2b79ff
+  }
+
+  100%{
+    color: white;
+  }
+`;
+const fadeInAnimation2 = keyframes`
+  0% {
+    opacity: 0;
+  }
+  50%{
+    opacity: 0;
+  }
+  100% {
     opacity: 1;
   }
 `;
@@ -32,33 +65,14 @@ const Container = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  margin-bottom: 30vh;
+
   @media (min-width: 768px) {
   }
 
   @media (min-width: 1024px) {
   }
 `;
-const FirstDiv = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-const SecondDiv = styled.div`
-  font-size: 2rem;
-  color: white;
-  margin-top: 2rem;
-  > p {
-    margin: 0;
-    @media (min-width: 768px) {
-      font-size: 1.5rem;
-    }
 
-    @media (min-width: 1024px) {
-      font-size: 2rem;
-    }
-  }
-`;
 const Column = styled.div`
   display: flex;
   > p {
@@ -67,7 +81,27 @@ const Column = styled.div`
   }
   > .fadeIn {
     margin: 0;
-    animation: ${fadeInAnimation} 2s ease-in-out;
+    animation: ${fadeInAnimation} 5s ease-in-out;
+  }
+`;
+
+const BtnColumnDiv = styled.div`
+  display: flex;
+  > button {
+    width: 140px;
+    height: 40px;
+    border-radius: 5px;
+    border: none;
+    background-color: rgba(50, 50, 50, 0.8);
+    color: white;
+    box-shadow: 1px 1px 3px 2px #000000;
+    margin: 50px 10px 0 10px;
+    animation: ${fadeInAnimation2} 2s ease-in-out;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 15px;
+    font-weight: bold;
   }
 `;
 
@@ -75,7 +109,7 @@ const TitleDiv = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
-  align-items: flex-start;
+  align-items: center;
   color: white;
   font-weight: bold;
   > p {
@@ -86,16 +120,5 @@ const TitleDiv = styled.div`
   }
   @media (min-width: 768px) {
     font-size: 4rem;
-  }
-`;
-
-const Img = styled.img`
-  width: 100%;
-  max-width: 300px;
-  height: auto;
-  margin-bottom: 2rem;
-
-  @media (min-width: 1024px) {
-    margin-right: 2rem;
   }
 `;
