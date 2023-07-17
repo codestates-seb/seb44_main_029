ce2e99eb25d88ba196de0a97db4d6e50079e408e
qpwoei0123 <qpwoei0123@naver.com>
2023-07-17 15:43:33 +0900

    Design: 페이지 다운 버튼

--- client/src/pages/MainPage.tsx
+++ client/src/pages/MainPage.tsx
@@ -1,16 +1,32 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import backgroundImg from '../assets/images/background.jpg';
 import Title from '../components/main/Title';
+import { BiChevronsDown } from 'react-icons/bi';
 
 const MainPage = () => {
   return (
     <Layout>
       <Title />
+      <AnimationDiv size="50px" color="white" />
     </Layout>
   );
 };
 
 export default MainPage;
+const fadeInAnimation = keyframes`
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+`;
+
+const moveUpDown = keyframes`
+  50% {
+    transform: translateY(-10px);
+  }
+`;
 
 const Layout = styled.div`
   width: 100%;
@@ -23,4 +39,11 @@ const Layout = styled.div`
   background-repeat: no-repeat;
   background-position: center;
   background-size: cover;
+`;
+
+const AnimationDiv = styled(BiChevronsDown)`
+  margin-bottom: 10px;
+  opacity: 0;
+  animation: ${fadeInAnimation} 1s ease-in-out 3s forwards,
+    ${moveUpDown} 2s ease-in-out infinite;
 `;
