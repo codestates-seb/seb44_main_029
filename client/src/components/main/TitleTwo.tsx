aacc2b33d2252cdec02700e2a2b87dbd6769896b
qpwoei0123 <qpwoei0123@naver.com>
2023-07-17 16:05:30 +0900

    Feat: 페이지 다운 기능

---
+++ client/src/components/main/TitleTwo.tsx
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+const TitleTwo = () => {
+  return <Container>hi</Container>;
+};
+
+export default TitleTwo;
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  @media (min-width: 768px) {
+  }
+
+  @media (min-width: 1024px) {
+  }
+`;
--- client/src/pages/MainPage.tsx
+++ client/src/pages/MainPage.tsx
@@ -1,18 +1,27 @@
 import styled, { keyframes } from 'styled-components';
-import backgroundImg from '../assets/images/background.jpg';
-import Title from '../components/main/Title';
+import TitleOne from '../components/main/TitleOne';
+import TitleTwo from '../components/main/TitleTwo';
 import { BiChevronsDown } from 'react-icons/bi';
 
 const MainPage = () => {
+  //다음 타이틀로 이동
+  const handleScrollDown = () => {
+    window.scrollTo({
+      top: window.innerHeight,
+      behavior: 'smooth',
+    });
+  };
   return (
     <Layout>
-      <Title />
-      <AnimationDiv size="50px" color="white" />
+      <TitleOne />
+      <TitleTwo />
+      <AnimationDiv size="50px" color="white" onClick={handleScrollDown} />
     </Layout>
   );
 };
 
 export default MainPage;
+//페이드 인
 const fadeInAnimation = keyframes`
   0% {
     opacity: 0;
@@ -30,18 +39,16 @@ const moveUpDown = keyframes`
 
 const Layout = styled.div`
   width: 100%;
-  height: 100vh;
+  height: 200vh;
   padding: 0;
   display: flex;
   align-items: center;
   flex-direction: column;
-  background-image: url(${backgroundImg});
-  background-repeat: no-repeat;
-  background-position: center;
-  background-size: cover;
 `;
 
 const AnimationDiv = styled(BiChevronsDown)`
+  position: absolute;
+  bottom: 0;
   margin-bottom: 10px;
   opacity: 0;
   animation: ${fadeInAnimation} 1s ease-in-out 3s forwards,
