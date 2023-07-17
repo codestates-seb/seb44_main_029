463591e57406770db356489f8860e57ba4c8e9b4
qpwoei0123 <qpwoei0123@naver.com>
2023-07-18 01:09:20 +0900

    Feat: 스크롤 감지 & 분기처리

--- client/src/components/main/TitleThree.tsx
+++ client/src/components/main/TitleThree.tsx
@@ -1,22 +1,58 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
-const TitleThree = () => {
-  return <Container>three</Container>;
+const TitleThree = ({ observer }: { observer: boolean }) => {
+  const onAnimationEnd = () => {
+    // 애니메이션 종료 후 처리할 작업을 여기에 추가하세요
+  };
+
+  return (
+    <Container observer={observer} onAnimationEnd={onAnimationEnd}>
+      <Box />
+    </Container>
+  );
 };
 
 export default TitleThree;
 
-const Container = styled.div`
+const slideInAnimation = keyframes`
+  0% {
+    transform: translateX(-100%);
+  }
+  100% {
+    transform: translateX(-30%);
+  }
+`;
+
+const slideOutAnimation = keyframes`
+  0% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 0;
+  }
+`;
+
+const Container = styled.div<{ observer: boolean }>`
   width: 100%;
   height: 100vh;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  color: white;
+  animation: ${({ observer }) =>
+      observer ? slideInAnimation : slideOutAnimation}
+    1s forwards;
+  color: ${({ observer }) => (observer ? 'red' : 'inherit')};
+  animation-fill-mode: both;
   @media (min-width: 768px) {
   }
 
   @media (min-width: 1024px) {
   }
+`;
+
+const Box = styled.div`
+  width: 300px;
+  height: 300px;
+  background-color: red;
 `;
--- client/src/components/main/TitleTwo.tsx
+++ client/src/components/main/TitleTwo.tsx
@@ -1,22 +1,62 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
-const TitleTwo = () => {
-  return <Container>two</Container>;
+interface TitleTwoProps {
+  observer: boolean;
+}
+
+const TitleTwo = ({ observer }: TitleTwoProps) => {
+  const onAnimationEnd = () => {
+    // 애니메이션 종료 후 처리할 작업을 여기에 추가하세요
+  };
+
+  return (
+    <Container observer={observer} onAnimationEnd={onAnimationEnd}>
+      <Box />
+    </Container>
+  );
 };
 
 export default TitleTwo;
 
-const Container = styled.div`
+const slideInAnimation = keyframes`
+  0% {
+    transform: translateX(-100%);
+  }
+  100% {
+    transform: translateX(0);
+  }
+`;
+
+const slideOutAnimation = keyframes`
+  0% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 0;
+  }
+`;
+
+const Container = styled.div<{ observer: boolean }>`
   width: 100%;
   height: 100vh;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  color: white;
+  animation: ${({ observer }) =>
+      observer ? slideInAnimation : slideOutAnimation}
+    1s forwards;
+  color: ${({ observer }) => (observer ? 'red' : 'inherit')};
+  animation-fill-mode: both;
   @media (min-width: 768px) {
   }
 
   @media (min-width: 1024px) {
   }
+`;
+
+const Box = styled.div`
+  width: 300px;
+  height: 300px;
+  background-color: red;
 `;
