e4926706fc4b66da2ea71c11d320eb0cf752fc77
qpwoei0123 <qpwoei0123@naver.com>
2023-07-17 16:20:01 +0900

    Design: 메인페이지 추가 타이틀 세팅

--- client/src/pages/MainPage.tsx
+++ client/src/pages/MainPage.tsx
@@ -1,7 +1,9 @@
 import styled, { keyframes } from 'styled-components';
 import TitleOne from '../components/main/TitleOne';
 import TitleTwo from '../components/main/TitleTwo';
+import TitleThree from '../components/main/TitleThree';
 import { BiChevronsDown } from 'react-icons/bi';
+import backgroundImg from '../assets/images/background.jpg';
 
 const MainPage = () => {
   //다음 타이틀로 이동
@@ -15,13 +17,14 @@ const MainPage = () => {
     <Layout>
       <TitleOne />
       <TitleTwo />
+      <TitleThree />
       <AnimationDiv size="50px" color="white" onClick={handleScrollDown} />
     </Layout>
   );
 };
 
 export default MainPage;
-//페이드 인
+//페이드 인 애니메이션
 const fadeInAnimation = keyframes`
   0% {
     opacity: 0;
@@ -30,7 +33,7 @@ const fadeInAnimation = keyframes`
     opacity: 1;
   }
 `;
-
+// 위아래 이동 애니메이션
 const moveUpDown = keyframes`
   50% {
     transform: translateY(-10px);
@@ -39,11 +42,15 @@ const moveUpDown = keyframes`
 
 const Layout = styled.div`
   width: 100%;
-  height: 200vh;
+  height: 300vh;
   padding: 0;
   display: flex;
   align-items: center;
   flex-direction: column;
+  background-image: url(${backgroundImg});
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
 `;
 
 const AnimationDiv = styled(BiChevronsDown)`
