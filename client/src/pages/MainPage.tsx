d75635334c68486d25f78cb88fc5f89d6f71f965
qpwoei0123 <qpwoei0123@naver.com>
2023-07-18 01:09:20 +0900

    Feat: 스크롤 감지 & 분기처리

--- client/src/pages/MainPage.tsx
+++ client/src/pages/MainPage.tsx
@@ -4,8 +4,10 @@ import TitleTwo from '../components/main/TitleTwo';
 import TitleThree from '../components/main/TitleThree';
 import { BiChevronsDown } from 'react-icons/bi';
 import backgroundImg from '../assets/images/background.jpg';
+import { useEffect, useState } from 'react';
 
 const MainPage = () => {
+  const [observer, serObserver] = useState(1);
   //다음 타이틀로 이동
   const handleScrollDown = () => {
     window.scrollTo({
@@ -13,11 +15,32 @@ const MainPage = () => {
       behavior: 'smooth',
     });
   };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      const windowHeight = window.innerHeight;
+      const documentHeight = document.documentElement.scrollHeight;
+      const scrollRatio = (scrollPosition + windowHeight) / documentHeight;
+
+      // 현재 스크롤 위치와 화면 및 문서의 세로 길이를 출력
+      if (scrollRatio > 0.8) {
+        serObserver(3);
+      } else if (scrollRatio > 0.5) {
+        serObserver(2);
+      } else {
+        serObserver(1);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+  }, []);
+
   return (
     <Layout>
       <TitleOne />
-      <TitleTwo />
-      <TitleThree />
+      <TitleTwo observer={observer === 2 ? true : false} />
+      <TitleThree observer={observer === 3 ? true : false} />
       <AnimationDiv size="50px" color="white" onClick={handleScrollDown} />
     </Layout>
   );
