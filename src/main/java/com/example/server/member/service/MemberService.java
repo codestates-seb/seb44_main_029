90cade35d4c4fcdb4e9a645db6254eea02eb834f
Lee dong yeol <96255906+11dy@users.noreply.github.com>
2023-07-18 11:34:59 +0900

    Update MemberService.java

--- src/main/java/com/example/server/member/service/MemberService.java
+++ src/main/java/com/example/server/member/service/MemberService.java
@@ -16,7 +16,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.data.redis.core.RedisTemplate;
+// import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.Authentication;
