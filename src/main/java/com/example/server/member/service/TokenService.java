6468ed39976293f2f8b01f407a2321366a27e69b
Lee dong yeol <96255906+11dy@users.noreply.github.com>
2023-07-18 11:34:16 +0900

    Update TokenService.java

--- src/main/java/com/example/server/member/service/TokenService.java
+++ src/main/java/com/example/server/member/service/TokenService.java
@@ -10,7 +10,7 @@ import com.example.server.member.security.token.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.data.redis.core.RedisTemplate;
+// import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.stereotype.Service;
 
