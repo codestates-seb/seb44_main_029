230d1b8d78bfae9dd7979bb6d6c7d6c227882e83
Lee dong yeol <96255906+11dy@users.noreply.github.com>
2023-07-18 11:39:26 +0900

    Update JwtFilter.java

--- src/main/java/com/example/server/member/security/filter/JwtFilter.java
+++ src/main/java/com/example/server/member/security/filter/JwtFilter.java
@@ -10,7 +10,7 @@ import io.jsonwebtoken.Jwts;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.RedisTemplate;
+// import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
