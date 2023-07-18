c9e753c434e3fe17805002dc9d19249560e384a0
RRDV04 <rrendezvous04@gmail.com>
2023-07-19 01:00:38 +0900

    Revert "Fix: Error 해결"
    This reverts commit a3fd063c5757d3c472f3f6ffa258c71a3dd80478.

--- src/main/java/com/example/server/likes/controller/LikeController.java
+++ src/main/java/com/example/server/likes/controller/LikeController.java
@@ -5,6 +5,7 @@ import com.example.server.likes.service.LikesServiceImpl;
 import com.example.server.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
