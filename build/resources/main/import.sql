d5f2167467158ccae80897f72d3161014b14799e
RRDV04 <rrendezvous04@gmail.com>
2023-07-20 10:56:56 +0900

    Fix: 파일 업로드 경로 변경 및 picture/thumbnail 업로드 분리, 폴더 분리

--- build/resources/main/import.sql
+++ build/resources/main/import.sql
@@ -1,7 +1,7 @@
-INSERT INTO theme (title) VALUES ('A');
-INSERT INTO theme (title) VALUES ('B');
-INSERT INTO theme (title) VALUES ('C');
-INSERT INTO theme (title) VALUES ('D');
-INSERT INTO theme (title) VALUES ('E');
+INSERT INTO theme (title) VALUES ('Nature');
+INSERT INTO theme (title) VALUES ('Space');
+INSERT INTO theme (title) VALUES ('Retro');
+INSERT INTO theme (title) VALUES ('Pixel');
+INSERT INTO theme (title) VALUES ('Animal');
 
 INSERT INTO member (active, username, role) VALUES (true, "admin", "ROLE_ADMIN");
