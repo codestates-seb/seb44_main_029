6dd1dda76a12fbf170ac536de89bdeabded6980e
Lee dong yeol <96255906+11dy@users.noreply.github.com>
2023-07-20 16:04:58 +0900

    Update HttpInterceptor.java

--- src/main/java/com/example/server/member/interceptor/HttpInterceptor.java
+++ src/main/java/com/example/server/member/interceptor/HttpInterceptor.java
@@ -6,8 +6,6 @@ import io.jsonwebtoken.ExpiredJwtException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
-<<<<<<< HEAD
-=======
 import org.springframework.security.core.Authentication;
 >>>>>>> a6e2750d6861ac975a3373930f4d76576af22e27
 import org.springframework.web.servlet.HandlerInterceptor;
