84bf931658ba7b0f1aa49290f626d7bb24d84a6e
qpwoei0123 <qpwoei0123@naver.com>
2023-07-21 13:59:49 +0900

    Feat: 호버시 화살표아이콘 추가

--- client/src/components/theme/themeList/ThemeCarousel.tsx
+++ client/src/components/theme/themeList/ThemeCarousel.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled, { keyframes } from 'styled-components';
 import { useNavigate } from 'react-router-dom';
-
+import { FiChevronsLeft, FiChevronsRight } from 'react-icons/fi';
 interface ThemeCarouselProps {
   imgList: string[];
   currentThemeIndex: number;
@@ -28,24 +28,52 @@ const ThemeCarousel = ({
 
   return (
     <Container>
-      <PreviousThemeImg
-        onClick={handlePreviousTheme}
-        src={imgList[(currentThemeIndex - 1 + imgList.length) % imgList.length]}
-      />
+      <PreviousDiv>
+        <FiChevronsLeft />
+        <PreviousThemeImg
+          onClick={handlePreviousTheme}
+          src={
+            imgList[(currentThemeIndex - 1 + imgList.length) % imgList.length]
+          }
+        />
+      </PreviousDiv>
       <CurrentThemeImg
         src={imgList[currentThemeIndex]}
         onClick={() => {
           navigate(`/theme/${currentThemeIndex + 1}`);
         }}
       />
-      <NextThemeImg
-        onClick={handleNextTheme}
-        src={imgList[(currentThemeIndex + 1) % imgList.length]}
-      />
+      <NextDiv>
+        <FiChevronsRight />
+        <NextThemeImg
+          onClick={handleNextTheme}
+          src={imgList[(currentThemeIndex + 1) % imgList.length]}
+        />
+      </NextDiv>
     </Container>
   );
 };
 export default ThemeCarousel;
+
+//오른쪽으로 이동
+const moveLeftRight = keyframes`
+  0%,100% {
+    transform: translateX(0); 
+  }
+  50% {
+    transform: translateX(2px); 
+  }
+`;
+
+//왼쪽으로 이동
+const moveLeftLeft = keyframes`
+  0%,100% {
+    transform: translateX(0);
+  }
+  50% {
+    transform: translateX(-2px); 
+  }
+`;
 
 //일렁이는 애니메이션
 const zoomAnimation = keyframes`
@@ -128,5 +156,69 @@ const NextThemeImg = styled.img`
   //호버 시 투명도 사라짐
   &:hover {
     opacity: 1;
+  }
+`;
+
+const PreviousDiv = styled.div`
+  display: flex;
+  align-items: center;
+  &:hover {
+    :first-child {
+      opacity: 1;
+    }
+  }
+  //화실표 아이콘
+  > :first-child {
+    opacity: 0;
+    position: absolute;
+    z-index: 200;
+    scale: 5;
+    left: 2%;
+    color: white;
+    animation: ${moveLeftLeft} 2s linear infinite;
+    @media (min-width: 300px) {
+      scale: 2;
+    }
+    @media (min-width: 768px) {
+      scale: 3;
+    }
+    @media (min-width: 1024px) {
+      scale: 4;
+    }
+  }
+  > :last-child {
+    height: 100%;
+  }
+`;
+
+const NextDiv = styled.div`
+  display: flex;
+  align-items: center;
+  &:hover {
+    :first-child {
+      opacity: 1;
+    }
+  }
+  //화실표 아이콘
+  > :first-child {
+    opacity: 0;
+    position: absolute;
+    z-index: 200;
+    scale: 5;
+    right: 2%;
+    color: white;
+    animation: ${moveLeftRight} 2s linear infinite;
+    @media (min-width: 300px) {
+      scale: 2;
+    }
+    @media (min-width: 768px) {
+      scale: 3;
+    }
+    @media (min-width: 1024px) {
+      scale: 4;
+    }
+  }
+  > :last-child {
+    height: 100%;
   }
 `;
