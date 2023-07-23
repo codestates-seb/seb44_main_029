8b5b9d0f83d6f52fbacd5ae1ba1c46cfec465a5a
dlwlgy <dlwlgy81@gmail.com>
2023-07-23 14:26:56 +0900

    design: 메인 화면으로 안내하는 문구 추가

--- client/src/error/NotFound.tsx
+++ client/src/error/NotFound.tsx
@@ -12,7 +12,10 @@ const NotFound = () => {
           페이지가 존재하지 않거나, 사용할 수 없는 페이지입니다. <br />
           입력하신 주소가 정확한지 다시 한 번 확인해주세요.
         </p>
-        <MainPageMoveLink to={`/`}>메인 화면</MainPageMoveLink>
+        <PageLinkWrapper>
+          <p>CozyStates 메인 화면으로 가기 →</p>
+          <MainPageMoveLink to={`/`}>메인 화면</MainPageMoveLink>
+        </PageLinkWrapper>
       </Container>
     </Layout>
   );
@@ -25,7 +28,7 @@ const Layout = styled.div`
   height: 100vh;
   display: flex;
   justify-content: center;
-  color: white;
+  color: rgba(255, 255, 255, 1);
   background: linear-gradient(180deg, rgba(0, 0, 0, 0.6), rgb(0, 0, 0, 1));
 `;
 
@@ -39,13 +42,22 @@ const Container = styled.div`
   }
 `;
 
+const PageLinkWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  > p {
+    color: rgba(255, 255, 255, 0.7);
+  }
+`;
+
 const MainPageMoveLink = styled(Link)`
   width: 6rem;
   height: 2rem;
   border-radius: 0.3rem;
   text-decoration: none;
   color: rgba(255, 255, 255, 1);
-
   background-color: rgba(50, 50, 50, 0.8);
   box-shadow: 1px 1px 0px 2px #000000;
   display: flex;
