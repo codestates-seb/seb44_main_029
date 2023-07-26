9f70a5bd1745afbdb5baa28439e215acda70c156
RRDV04 <rrendezvous04@gmail.com>
2023-07-26 16:17:38 +0900

    Feat: Transaction, DB lock

--- src/main/java/com/example/server/likes/repository/LikeRepository.java
+++ src/main/java/com/example/server/likes/repository/LikeRepository.java
@@ -4,11 +4,15 @@ import com.example.server.content.entity.Content;
 import com.example.server.likes.entity.Likes;
 import com.example.server.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.LockModeType;
 import java.util.Optional;
 
 @Repository
 public interface LikeRepository extends JpaRepository<Likes, Long> {
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     Optional<Likes> findByMemberAndContent(Member member, Content content);
 }
