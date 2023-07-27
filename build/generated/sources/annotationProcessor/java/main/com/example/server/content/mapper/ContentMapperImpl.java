a525ffddb791c7071a633fa548c26b758c7d8173
Ubuntu <ubuntu@ip-172-31-8-26.ap-northeast-2.compute.internal>
2023-07-27 07:33:20 +0000

    최신버전 push

--- build/generated/sources/annotationProcessor/java/main/com/example/server/content/mapper/ContentMapperImpl.java
+++ build/generated/sources/annotationProcessor/java/main/com/example/server/content/mapper/ContentMapperImpl.java
@@ -5,8 +5,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-07-24T17:30:34+0900",
-    comments = "version: 1.5.3.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-8.1.1.jar, environment: Java 11.0.18 (Azul Systems, Inc.)"
+    date = "2023-07-26T14:35:25+0000",
+    comments = "version: 1.5.3.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-8.1.1.jar, environment: Java 11.0.19 (Ubuntu)"
 )
 @Component
 public class ContentMapperImpl implements ContentMapper {
